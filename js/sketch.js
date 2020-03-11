ab212e90009a1e4add7c562dc8593c5d01b3d77a
llaesser <60931177+llaesser@users.noreply.github.com>
2020-03-11 14:48:32 +0100

    color and stroke

--- js/sketch.js
+++ js/sketch.js
@@ -9,7 +9,13 @@ function draw() {
   if (mouseIsPressed){
 	  line(mouseX,mouseY,550,300);
 	  stroke(255, 0, 0)  }
-	  
+	  fill(0);
+	  if (mouseIsPressed){
+		  line(mouseX,mouseY,550,600);
+		  stroke(0, 0, 255)  }
+		  strokeWeight(9);
+		  
+
 }
 
 function windowResized() {
