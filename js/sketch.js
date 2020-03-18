08bad993ab33ed7b6029185fcf1000ba51624ce2
llaesser <60931177+llaesser@users.noreply.github.com>
2020-03-18 14:17:19 +0100

    else black line

--- js/sketch.js
+++ js/sketch.js
@@ -1,33 +1,39 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
-  noStroke();
-  pixelDensity(2);
+    createCanvas(windowWidth, windowHeight);
+    background(255);
+    noStroke();
+    pixelDensity(2);
 
 
 
 }
 
 function draw() {
-  fill(0);
-  if (mouseIsPressed){
-	  line(mouseX,mouseY,550,300);
-	  stroke(255, 0, 0)  }
-	  fill(0);
+    fill(0);
+    if (mouseIsPressed) {
+        line(mouseX, mouseY, 550, 300);
+        stroke(255, 0, 0)
+        line(mouseX, mouseY, 550, 600);
+        stroke(0, 0, 255)
+    } else {
+        line(mouseX, mouseY, 550, 300);
+        stroke(0, 0, 0)
 
-	  if (mouseIsPressed){
-		  line(mouseX,mouseY,550,600);
-		  stroke(0, 0, 255)  }
-		  strokeWeight(4);
-		  cursor(CROSS, 8, 20)
-		  
+    }
+    fill(0);
+
+
+    strokeWeight(4);
+    cursor(CROSS, 8, 20)
+
+
 
 
 
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  background(0);
+    resizeCanvas(windowWidth, windowHeight);
+    background(0);
 
 }
